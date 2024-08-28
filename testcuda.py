958561bd5c28e298e4c2a2afca46c2666b88d8b1
sitthatot <sitthatot@gmail.com>
2024-08-28 16:55:17 +0700

    train model

--- testcuda.py
+++ testcuda.py
@@ -1,3 +1,5 @@
-import torch
-print(torch.cuda.device_count())
-print(torch.cuda.get_device_name(0))
+# import torch
+# print(torch.cuda.device_count())
+# print(torch.cuda.get_device_name(0))
+import os
+print(os.cpu_count())
